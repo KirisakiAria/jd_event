2ae04aadf904f7b19fc82d84ab37fc90ed04b991
伟大鱼塘 <xiaoli350791904@hotmail.com>
2020-05-22 13:27:22 +0800

    Update 🎂.js
    修复一些问题

--- 🎂.js
+++ 🎂.js
@@ -3,22 +3,41 @@ toast('开始执行')
 let btnIndex = 0 //跳过好友助力
 let commodityViewCount = 0 //浏览商品浏览计数
 let cartCount = 0 //架构计数
-const sleepTime = 12000 //等待8s任务场景的等待时间（手机性能差建议调长一些）
-const sleepTime_2 = 20000 //有些场景加载很慢，建议设置长一些
+const interval = 4000 //任务执行间隔，手机性能差的弄长一点
+const sleepTime = 20000 //有些场景加载很慢，建议设置长一些
 if (className('android.view.View').textContains('邀请好友助力').exists()) {
   btnIndex = 1
 }
 
 const task = () => {
-  if (text('去完成').exists()) {
+  //8s任务
+  if (textContains('恭喜完成，获得').exists()) {
+    if (id('com.jingdong.app.mall:id/fe').exists()) {
+      id('com.jingdong.app.mall:id/fe').findOne().click()
+    } else if (id('com.jingdong.app.mall:id/fd').exists()) {
+      id('com.jingdong.app.mall:id/fd').findOne().click()
+    } else {
+      back()
+    }
+  } else if (text('去完成').exists()) {
     //任务页
     click('去完成', btnIndex)
   } else if (text('浏览以下5个商品').exists()) {
     //商品浏览
-    if (commodityViewCount >= 5) {
+    if (commodityViewCount >= 6) {
       //任务完成
       commodityViewCount = 0
-      id('fe').findOne().click()
+      if (id('com.jingdong.app.mall:id/fe').exists()) {
+        id('com.jingdong.app.mall:id/fe').findOne().click()
+      } else if (id('com.jingdong.app.mall:id/fd').exists()) {
+        id('com.jingdong.app.mall:id/fd').findOne().click()
+      } else if (id('fe').exists()) {
+        id('fe').findOne().click()
+      } else if (id('fd').exists()) {
+        id('fd').findOne().click()
+      } else {
+        back()
+      }
     }
     className('android.view.View')
       .text('浏览以下5个商品')
@@ -30,10 +49,21 @@ const task = () => {
       .click()
     commodityViewCount++
   } else if (text('当前页点击加购以下5个商品').exists()) {
-    if (cartCount >= 5) {
+    //加购
+    if (cartCount >= 6) {
       //任务完成
       cartCount = 0
-      id('fe').findOne().click()
+      if (id('com.jingdong.app.mall:id/fe').exists()) {
+        id('com.jingdong.app.mall:id/fe').findOne().click()
+      } else if (id('com.jingdong.app.mall:id/fd').exists()) {
+        id('com.jingdong.app.mall:id/fd').findOne().click()
+      } else if (id('fe').exists()) {
+        id('fe').findOne().click()
+      } else if (id('fd').exists()) {
+        id('fd').findOne().click()
+      } else {
+        back()
+      }
     }
     className('android.view.View')
       .text('当前页点击加购以下5个商品')
@@ -45,37 +75,45 @@ const task = () => {
       .child(2)
       .click()
     cartCount++
-  }
-  //商品页
-  else if (text('购物车').exists() && text('店铺').exists()) {
-    sleep(3000)
-    id('fe').findOne().click()
-  } else if (className('android.view.View').textContains('S').exists()) {
-    //8秒任务浏览
-    sleep(sleepTime)
-    id('fe').findOne().click()
+  } else if (text('购物车').exists() && text('店铺').exists()) {
+    //商品页
+    sleep(4000)
+    if (id('com.jd.lib.productdetail:id/fe').exists()) {
+      id('com.jd.lib.productdetail:id/fe').findOne().click()
+    } else if (id('com.jd.lib.productdetail:id/fd').exists()) {
+      id('com.jd.lib.productdetail:id/fd').findOne().click()
+    } else if (id('fe').exists()) {
+      id('fe').findOne().click()
+    } else if (id('fd').exists()) {
+      id('fd').findOne().click()
+    } else {
+      back()
+    }
   } else {
     //其他的一些浏览任务
-    sleep(sleepTime_2)
-    if (id('fd').exists()) {
-      //领卷中心等
+    sleep(sleepTime)
+    if (id('com.jingdong.app.mall:id/fe').exists()) {
+      id('com.jingdong.app.mall:id/fe').findOne().click()
+    } else if (id('com.jingdong.app.mall:id/fd').exists()) {
+      id('com.jingdong.app.mall:id/fd').findOne().click()
+    } else if (id('fe').exists()) {
+      id('fe').findOne().click()
+    } else if (id('fd').exists()) {
       id('fd').findOne().click()
+    } else {
+      back()
     }
     if (className('android.widget.Button').desc('返回').exists()) {
       //领京豆页面等
       className('android.widget.Button').desc('返回').findOne().click()
     }
-    if (className('android.view.View').textContains('S').exists()) {
-      //这里是一些特殊的8秒浏览，比如城市红包、宠物这种
-      sleep(sleepTime)
-      id('fe').findOne().click()
-    } else {
-      id('fe').findOne().click()
+    if (className('android.view.ViewGroup').desc('返回按钮').exists()) {
+      className('android.view.ViewGroup').desc('返回按钮').findOne().click()
     }
   }
 }
 for (;;) {
-  sleep(3000)
+  sleep(interval)
   const btnCollection = text('去完成').find()
   //任务完成 跳出循环
   if (btnCollection.length == 1 && btnIndex == 1) {
