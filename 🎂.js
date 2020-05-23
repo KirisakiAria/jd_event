50d0a248b482cd11e21a8fc4376f6268af4e0e5b
liguotian <li.guotian@vpclub.cn>
2020-05-23 15:53:58 +0800

    1.调整了一些页面返回的逻辑 2.现在从首页就可以运行脚本自动进入活动

--- 🎂.js
+++ 🎂.js
@@ -1,10 +1,11 @@
 toast('开始执行')
 
-let btnIndex = 0 //跳过好友助力
+let btnIndex = 0 //跳过一些无法完成的任务
 let commodityViewCount = 0 //浏览商品计数
 let cartCount = 0 //加购计数
 const interval = 4000 //任务执行间隔，手机性能差的设置大一些
 const sleepTime = 20000 //有些场景加载得很慢，建议设置大一些
+const version = device.release //安卓版本
 
 const judge = () => {
   if (className('android.view.View').textContains('邀请好友助力').exists()) {
@@ -13,8 +14,24 @@ const judge = () => {
 }
 
 const task = () => {
+  if (
+    className('android.widget.TextView').text('扫啊扫').exists() &&
+    className('android.widget.TextView').text('消息').exists()
+  ) {
+    //从首页自动进入活动
+    if (parseInt(version.substring(0, 1)) >= 7) {
+      const w1 = className('android.widget.ImageView')
+        .descContains('浮层活动')
+        .findOne()
+      click(w1.bounds().centerX(), w1.bounds().centerY())
+      sleep(sleepTime)
+      idContains('homeSceneBtnTask').findOne().click()
+    }
+  }
   //跳出组队任务
-  if (className('android.view.View').text('继续领红包').depth(12).exists()) {
+  else if (
+    className('android.view.View').text('继续领红包').depth(12).exists()
+  ) {
     //任务列表页返回确认对话框
     className('android.view.View')
       .text('继续领红包')
@@ -33,8 +50,6 @@ const task = () => {
       idContains('fe').findOne().click()
     } else if (idContains('fd').exists()) {
       idContains('fd').findOne().click()
-    } else {
-      back()
     }
     sleep(4000)
     btnIndex = 2
@@ -128,17 +143,30 @@ const task = () => {
       idContains('pop-start-btn').exists()
     ) {
       //游戏
-      sleep(2000)
       idContains('pop-start-btn').findOne().click()
       idContains('pop-fail2-btn').waitFor()
       idContains('pop-fail2-btn').findOne().click()
     } else if (textContains('东东萌宠').exists()) {
       // 萌宠
-      back()
+      if (parseInt(version.substring(0, 1)) >= 7) {
+        toast()
+        const w2 = className('android.view.ViewGroup')
+          .desc('返回按钮')
+          .findOne()
+          .findOne()
+        click(w2.bounds().centerX(), w2.bounds().centerY())
+      } else {
+        back()
+      }
     }
     if (className('android.widget.TextView').textContains('领京豆').exists()) {
       // 领京豆
-      back()
+      if (parseInt(version.substring(0, 1)) >= 7) {
+        const w3 = className('android.widget.Button').desc('返回').findOne()
+        click(w3.bounds().centerX(), w3.bounds().centerY())
+      } else {
+        back()
+      }
     } else if (className('android.view.ViewGroup').desc('返回按钮').exists()) {
       className('android.view.ViewGroup').desc('返回按钮').findOne().click()
     } else if (idContains('com.jingdong.app.mall:id/fe').exists()) {
